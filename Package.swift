ba110d810983c982351748fdaea0669b2654adaa
Aaron Tyler <aaron@atomicrobot.com>
2020-12-09 11:05:43 -0500

    Copied changes from https://github.com/maniramezan/DateTools to fix errors

--- Package.swift
+++ Package.swift
@@ -1,27 +1,39 @@
-// swift-tools-version:5.2
-// The swift-tools-version declares the minimum version of Swift required to build this package.
+// swift-tools-version:5.3
 
 import PackageDescription
 
 let package = Package(
     name: "DateToolsSwift",
+    platforms: [
+        .macOS(.v10_10), .iOS(.v8), .tvOS(.v9), .watchOS(.v2)
+    ],
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "DateToolsSwift",
-            targets: ["DateToolsSwift"]),
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+            targets: ["DateToolsSwift"])
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "DateToolsSwift",
-            dependencies: [],
-            path: "DateToolsSwift/DateTools"
+            path: "DateToolsSwift/DateTools",
+            exclude: [
+                "Examples",
+                "DateToolsSwift/Examples"
+            ],
+            resources: [
+                .copy("DateTools.bundle")
+            ]
         ),
+        .testTarget(
+            name: "DateToolsSwiftTests",
+            dependencies: ["DateToolsSwift"],
+            path: "DateToolsSwift/Tests/DateToolsTests/DateToolsTestsTests",
+            exclude: [
+                "Info.plist"
+            ],
+            resources: [
+                .copy("DateTools.bundle")
+            ]
+        )
     ]
 )
